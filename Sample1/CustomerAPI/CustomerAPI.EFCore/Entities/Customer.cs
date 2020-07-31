afbb134a42f35fb86362bbe5b699c20616a40418
Programming Goldflash <philip.ainberger@aon.at>
2020-07-31 17:45:15 +0200

    added ef-core entities

---
+++ Sample1/CustomerAPI/CustomerAPI.EFCore/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomerAPI.EFCore.Entities
+{
+    public class Customer
+    {
+        #region Public Properties
+
+        public DateTime Created { get; set; }
+        public string FirstName { get; set; }
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Updated { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+
+        #endregion Public Properties
+    }
+}
---
+++ Sample1/CustomerAPI/CustomerAPI.EFCore/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerAPI.EFCore.Entities
+{
+    public class User
+    {
+        #region Public Properties
+
+        public int Id { get; set; }
+        public string PasswordHash { get; set; }
+        public string Username { get; set; }
+
+        #endregion Public Properties
+    }
+}
