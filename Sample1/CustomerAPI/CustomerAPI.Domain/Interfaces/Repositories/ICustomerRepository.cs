06c955e0009d524c655346942947f6ebc173f216
Programming Goldflash <philip.ainberger@aon.at>
2020-07-31 17:41:27 +0200

    added custom repositories

---
+++ Sample1/CustomerAPI/CustomerAPI.Domain/Interfaces/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerAPI.Domain.Interfaces.Repositories
+{
+    public interface ICustomerRepository<T> : IRepository<T> where T : class
+    {
+        bool CheckForEmailAsync(string email);
+    }
+}
