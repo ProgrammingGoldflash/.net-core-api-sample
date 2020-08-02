c5f1d39635a5774d26bb215ab9c94fea47c11a4a
Programming Goldflash <philip.ainberger@aon.at>
2020-08-02 17:14:36 +0200

    unit of work - thoughts

---
+++ Sample1/CustomerAPI/CustomerAPI.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using CustomerAPI.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerAPI.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Commit();
+    }
+}
---
+++ Sample1/CustomerAPI/CustomerAPI.EFCore/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using CustomerAPI.Domain.Interfaces;
+using CustomerAPI.EFCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerAPI.EFCore
+{
+    public class DatabaseContext : DbContext, IUnitOfWork
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseContext() : base()
+        {
+
+        }
+        
+        public bool Commit()
+        {
+            return this.SaveChanges() == 0;
+        }
+    }
+}
